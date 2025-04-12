3ec304e06b2626aefb3faedea2b404056e220d43
May Thazin Htun <maythazinhtunn@gmail.com>
2025-04-12 22:05:15 +0630

    create .env and run pipeline

--- week9_demo/etl_demo.py
+++ week9_demo/etl_demo.py
@@ -116,8 +116,9 @@ def transform_data():
     
 
 def load_data(df:pd.DataFrame):
-    db_path = os.getenv("sqlite")
-    db_url = "sqlite:///" + os.path.abspath(db_path)
+    db_path = os.getenv("sqlite", "week9_demo/mmdt.db")
+    db_url = os.path.abspath(db_path)
+    # db_url = "sqlite:///" + os.path.abspath(db_path)
     print(f"Db url - {db_url}")
     engine = create_engine(db_url)
     df.to_sql("covid_city_demo", engine, if_exists="replace", index=False)
