bfb3e5382c58e366ea17a5e12e1722d42b1832f6
May Thazin Htun <maythazinhtunn@gmail.com>
2025-04-12 23:57:28 +0630

    add sqlite

--- week9_demo/etl_demo.py
+++ week9_demo/etl_demo.py
@@ -117,6 +117,7 @@ def transform_data():
 
 def load_data(df:pd.DataFrame):
     db_path = os.getenv("sqlite", "week9_demo/mmdt.db")
+    os.makedirs(os.path.dirname(db_path), exist_ok=True)
     db_url = "sqlite:///" + os.path.abspath(db_path)
     # db_url = "sqlite:///" + os.path.abspath(db_path)
     print(f"Db url - {db_url}")
